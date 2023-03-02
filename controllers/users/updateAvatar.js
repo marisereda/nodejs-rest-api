501732abf63317bb1e3065e9fc76678d5494259c
Marina Sereda <marina.gorb@gmail.com>
2023-03-02 17:55:57 +0200

    fix: fix updateAvatar controller

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -15,8 +15,8 @@ const updateAvatar = async (req, res) => {
     const avatar = await Jimp.read(tempUpload);
     await avatar.resize(250, 250).writeAsync(tempUpload);
     await fs.rename(tempUpload, avatarPath);
-    await User.findByIdAndUpdate(userId, { avatarURL });
-    res.status(200).json({ avatarURL });
+    const user = await User.findByIdAndUpdate(userId, { avatarURL }, { new: true });
+    res.status(200).json({ avatarURL: user.avatarURL });
   } catch (error) {
     await fs.unlink(tempUpload);
     throw new Unauthorized("Not authorized.");
